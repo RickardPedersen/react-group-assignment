6ee071386eb6567b848f9feb4078ca3c6e11fc06
faboosh <fabulo1998@gmail.com>
2020-11-04 16:18:59 +0100

    broke stuff

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
+import React, { useState } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import CustomerDetailPage from './pages/CustomerDetailPage'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
+import { CustomerContext } from './contexts/CustomerContext'
 
 function App() {
+  const [customerDataList, setCustomerDataList] = useState(null)
   return (
-    <div className="App">
+    <div>
       <Switch>
-        <Route exact path="/customer/:id" component={CustomerDetailPage}></Route>
-        <Route exact path="/home">
-          <HomePage/>
-        </Route>
+        <CustomerContext.Provider value={{customerDataList, setCustomerDataList}}>
+          <Route exact path="/customer/:id" component={CustomerDetailPage}></Route>
+          <Route exact path="/home">
+            <HomePage/>
+          </Route>
+        </CustomerContext.Provider>
         <Route exact path="/">
           <LoginPage/>
         </Route>
