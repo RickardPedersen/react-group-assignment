8f624faf8447249f98a1c85240eb5f2a7a1de283
faboosh <fabulo1998@gmail.com>
2020-11-05 12:53:54 +0100

    worked on CustomerDetailPage

--- src/pages/CustomerDetailPage.jsx
+++ src/pages/CustomerDetailPage.jsx
@@ -1,14 +1,36 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 import { CustomerContext } from '../contexts/CustomerContext'
+import { H3, P, Div } from '../components/partials/GeneralStyles'
+import Container from '../components/partials/Container'
+
 
 export default function CustomerDetailPage() {
+    const {id} = useParams();
+
     const { 
         customerDataList, 
-        setCustomerDataList 
     } = useContext(CustomerContext)
+    const [customer, setCustomer] = useState(null)
+    
+    useEffect(() => {
+        const index = customerDataList.findIndex(customer => customer.id == id);
+        setCustomer(customerDataList[index]);
+        console.log(index)
+    }, [])
+    
     return (
-        <div>
-
-        </div>
+        <Container>
+            {customer &&
+                <Div background="gray1" padding="1em" borderRadius="borderRadius">
+                    <H3>{customer.name}</H3>
+                    {Object.entries(customer).map(entry => {
+                        if(typeof entry[1] == 'string') {
+                            return <P>{`${entry[0]}: ${entry[1]}`}</P>
+                        }
+                    })}              
+                </Div>
+            }
+        </Container>
     )
 }
