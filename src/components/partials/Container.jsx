0cbf729c59c001aa6f73eadbc39048fc2905d7f3
Alexandre Previ <alexandre.previ@gmail.com>
2020-11-05 13:05:33 +0100

    styled-components/macro

--- src/components/partials/Container.jsx
+++ src/components/partials/Container.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components/macro";
 
 const StyledContainer = styled.div`
-	max-width: ${({ width }) => (width ? width : '800px')};
-	margin: 0 auto;
-	padding-top: 80px;
+  max-width: ${({ width }) => (width ? width : "800px")};
+  margin: 0 auto;
+  padding-top: 80px;
 `;
 
 export default function Container({ children, width }) {
-	return <StyledContainer width={width}>{children}</StyledContainer>;
+  return <StyledContainer width={width}>{children}</StyledContainer>;
 }
