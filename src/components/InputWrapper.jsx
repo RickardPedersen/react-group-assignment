a64d303ef41dd189146662100b680e6566c3c1b1
Bryan Santamaria <bryaan.santamaria@gmail.com>
2020-11-05 18:29:34 +0100

    moved inputWrapper into createCustomer Component

--- src/components/InputWrapper.jsx
+++ src/components/InputWrapper.jsx
@@ -1,24 +1,25 @@
-import React from "react";
+import React from 'react';
+import InputComponent from '../components/partials/InputComponent';
 
 export default function InputWrapper(props) {
-  const { formData, setFormData } = props;
-  function handleOnChange(e) {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  }
-  return (
-    <>
-      {Object.keys(formData).map((inputNameItem, index) => {
-        return (
-          <InputComponent
-            key={index}
-            type={"text"}
-            name={inputNameItem}
-            value={formData[inputNameItem]}
-            placeholder={`enter ${inputNameItem}`}
-            handleOnChange={handleOnChange}
-          />
-        );
-      })}
-    </>
-  );
+	const { formData, setFormData } = props;
+	function handleOnChange(e) {
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+	}
+	return (
+		<>
+			{Object.keys(formData).map((inputNameItem, index) => {
+				return (
+					<InputComponent
+						key={index}
+						type={'text'}
+						name={inputNameItem}
+						value={formData[inputNameItem]}
+						placeholder={`enter ${inputNameItem}`}
+						handleOnChange={handleOnChange}
+					/>
+				);
+			})}
+		</>
+	);
 }
